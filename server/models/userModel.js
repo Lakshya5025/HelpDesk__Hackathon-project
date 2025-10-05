a8df0c35e7855fb35a441fb95419f835b231a04c
Lakshya Gupta <lakshyagupta916640@gmail.com>
2025-10-05 11:39:18 +0530

    add password hashing

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -28,6 +28,14 @@ const userSchema = mongoose.Schema(
     }
 );
 
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) {
+        next();
+    }
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, process.env.SALT);
+});
+
 
 const User = mongoose.model('User', userSchema);
 
