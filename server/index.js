571f4a509885adf1c7a5352260ddcb86c9520c5f
Lakshya Gupta <lakshyagupta916640@gmail.com>
2025-10-05 11:22:09 +0530

    update type

---
+++ server/index.js
@@ -0,0 +1,31 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+
+const app = express();
+app.use(express.json());
+app.use(
+    cors({
+        origin: "http://localhost:5173",
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        credentials: true,
+    })
+);
+async function startServer() {
+    try {
+        await mongoose.connect(process.env.MONGO_URL);
+        console.log("DB connected successfully");
+
+        app.listen(process.env.PORT, () => {
+            console.log(`Server is listening on ${process.env.SERVER_URL}`);
+        });
+    } catch (err) {
+        console.error("Failed to connect to the database:");
+        console.error(err);
+        process.exit(1);
+    }
+}
+
+startServer();
