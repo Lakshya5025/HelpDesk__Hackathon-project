4a6b1c0882cc11575f21456ad38e67440b5a62db
Lakshya Gupta <lakshyagupta916640@gmail.com>
2025-11-02 17:55:01 +0530

    update smtp port

--- server/utils/email.js
+++ server/utils/email.js
@@ -7,8 +7,8 @@ dotenv.config();
 
 export const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
+    port: 465,
+    secure: true,
     auth: {
         user: process.env.EMAIL,
         pass: process.env.EMAIL_PASSWORD,
